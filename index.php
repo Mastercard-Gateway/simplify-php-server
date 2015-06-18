6b79723d0c0ae60591f6dc913b1e19e98f8e433e
Siddique Hameed <siddique_hameed@mastercard.com>
2015-06-18 12:00:07 -0500

    styling updates

--- index.php
+++ index.php
@@ -32,6 +32,11 @@
  <html lang="en">
  <head>
 	 <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+	 <style>
+		 .container {
+			 margin-top: 20px;
+		 }
+	 </style>
  </head>
 
  <body>
